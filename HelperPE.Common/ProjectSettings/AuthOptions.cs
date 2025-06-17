e4a4c8b69d8cf2d7ffca754a59cb837eae06dc7f
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-06-17 11:28:32 +0700

    add auth setup

---
+++ HelperPE.Common/ProjectSettings/AuthOptions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace HelperPE.Common.ProjectSettings
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "HelperPEServer";
+        public const string AUDIENCE = "HelperPEClient";
+        public const string KEY = "superKeyPuPuPu_52#Peterburg-eeeAaALike";
+        public const int LIFETIME_MINUTES = GeneralSettings.ACCESS_TOKEN_LIFETIME;
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() => 
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+    }
+}
---
+++ HelperPE.Common/ProjectSettings/GeneralSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace HelperPE.Common.ProjectSettings
+{
+    public class GeneralSettings
+    {
+        public const int ACCESS_TOKEN_LIFETIME = 10 * 3600; // in minutes
+        public const int REFRESH_TOKEN_LIFETIME = 24; // in hours
+    }
+}
