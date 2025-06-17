2c125f8eb1f7c9de9a368e28d018a8b0fdaabcca
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-06-17 11:32:29 +0700

    add custom exceptions

---
+++ HelperPE.Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace HelperPE.Common.Exceptions
+{
+    public class NotFoundException : CustomException
+    {
+        public NotFoundException(string message) :
+            base(StatusCodes.Status404NotFound, "Not found", message)
+        { }
+    }
+}
