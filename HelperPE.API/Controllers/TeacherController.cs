c063f1bfacabca800fed47295ec03db766ba41e2
Aleksey Zinovjev <alexz04tab2@gmail.com>
2025-06-29 18:18:05 +0700

    add teacher contoller documentation

--- HelperPE.API/Controllers/TeacherController.cs
+++ HelperPE.API/Controllers/TeacherController.cs
@@ -1,5 +1,7 @@
 ﻿using HelperPE.Application.Services;
 using HelperPE.Common.Constants;
+using HelperPE.Common.Models.Pairs;
+using HelperPE.Common.Models.Teacher;
 using HelperPE.Infrastructure.Filters;
 using HelperPE.Infrastructure.Utilities;
 using Microsoft.AspNetCore.Authorization;
@@ -19,6 +21,13 @@ namespace HelperPE.API.Controllers
             _teacherService = teacherService;
         }
 
+        /// <summary>
+        /// Get list of teacher pairs
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(TeacherPairsModel), StatusCodes.Status200OK)]
         [HttpGet("pairs")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
@@ -27,6 +36,15 @@ namespace HelperPE.API.Controllers
             return Ok(await _teacherService.GetTodayPairs(UserDescriptor.GetUserId(User)));
         }
 
+        /// <summary>
+        /// Create new pair
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <responde code="400">Validation errors</responde>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="404">Subject not found</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(TeacherPairsModel), StatusCodes.Status200OK)]
         [HttpPost("pairs")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
@@ -37,6 +55,13 @@ namespace HelperPE.API.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Approve student pair application with classes amount
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="404">Pair or user not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpPut("pairs/{pairId}")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
@@ -48,6 +73,13 @@ namespace HelperPE.API.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Reject student pair application
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="404">Pair or user not found</response>
+        /// <response code="500">Internal server error</response>
         [HttpDelete("pairs/{pairId}")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
@@ -59,6 +91,13 @@ namespace HelperPE.API.Controllers
             return Ok();
         }
 
+        /// <summary>
+        /// Get list of teacher subjects
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(SubjectListModel), StatusCodes.Status200OK)]
         [HttpGet("subjects")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
@@ -67,6 +106,13 @@ namespace HelperPE.API.Controllers
             return Ok(await _teacherService.GetTeacherSubjects(UserDescriptor.GetUserId(User)));
         }
 
+        /// <summary>
+        /// Get list of actual attendances for all pairs
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(PairAttendancesListModel), StatusCodes.Status200OK)]
         [HttpGet("attendances")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
@@ -76,6 +122,14 @@ namespace HelperPE.API.Controllers
                 (UserDescriptor.GetUserId(User)));
         }
 
+        /// <summary>
+        /// Get list of pending pair attendances for definite pair
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="404">Pair not found</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(PairAttendanceListShortModel), StatusCodes.Status200OK)]
         [HttpGet("attendances/pending/{pairId}")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
@@ -85,6 +139,14 @@ namespace HelperPE.API.Controllers
                 UserDescriptor.GetUserId(User))); 
         }
 
+        /// <summary>
+        /// Get list of solved pair attendances for definite pair
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="404">Pair not found</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(PairAttendanceListShortModel), StatusCodes.Status200OK)]
         [HttpGet("attendances/solved/{pairId}")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
@@ -94,6 +156,14 @@ namespace HelperPE.API.Controllers
                 UserDescriptor.GetUserId(User)));
         }
 
+        /// <summary>
+        /// Get list of all pair attendances for definite pair
+        /// </summary>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="404">Pair not found</response>
+        /// <response code="500">Internal server error</response>
+        [ProducesResponseType(typeof(PairAttendanceListShortModel), StatusCodes.Status200OK)]
         [HttpGet("attendances/all/{pairId}")]
         [Authorize(Roles = RolesCombinations.TEACHER_AND_CURATOR)]
         [CheckTokens]
